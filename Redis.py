46d32821b7c5bd9e31251efceb3323a0d7a88383
DaarkGms <lucaspiresrv@hotmail.com>
2023-08-22 08:45:12 -0300

    Contador

--- Redis.py
+++ Redis.py
@@ -4,24 +4,35 @@ import os
 class TaskManager:
     def __init__(self):
         self.redis_client = redis.Redis(host='localhost', port=6379, db=0)
+        self.task_counter_key = 'task_counter'
+        self.last_task_id = int(self.redis_client.get(self.task_counter_key) or 0)
 
-    def add_task(self, description):
-        task_id = self.redis_client.incr('task_counter')
+    def add_task(self, description): 
+        self.last_task_id += 1
+        task_id = self.last_task_id  
         self.redis_client.set(f'task:{task_id}', description)
-        print(f'Tarefa {task_id} adiconada com successo.')
+        print(f'Tarefa {task_id} adicionada com sucesso.')
+
+        self.redis_client.set(self.task_counter_key, self.last_task_id)  
 
     def listar_tarefas(self):
-        task_ids = self.redis_client.keys('task:*')
-        for task_key in task_ids:
-            task_id = task_key.decode('utf-8').split(':')[1]
-            description = self.redis_client.get(task_key).decode('utf-8')
-            print(f'ID da tarefa: {task_id}, Descrição: {description}')
+     task_ids = self.redis_client.keys('task:*')
+     sorted_task_keys = sorted(task_ids, key=lambda x: int(x.decode('utf-8').split(':')[1]))
+
+     for task_key in sorted_task_keys:
+        task_id = task_key.decode('utf-8').split(':')[1]
+        description = self.redis_client.get(task_key).decode('utf-8')
+        print(f'ID da tarefa: {task_id}; Descrição: {description}')
+
 
     def remove_tarefa(self, task_id):
         task_key = f'task:{task_id}'
         if self.redis_client.exists(task_key):
             self.redis_client.delete(task_key)
             print(f'Tarefa com ID {task_id} removida com sucesso.')
+            if int(task_id) == self.last_task_id:
+                self.last_task_id -= 1
+                self.redis_client.set(self.task_counter_key, self.last_task_id)  # Atualize o contador
         else:
             print(f'Tarefa {task_id} não encontrada.')
 
@@ -35,7 +46,6 @@ if __name__ == '__main__':
         print("4. Sair")
         
         opcao = input("Informe sua escolha: ")
-        os.system('cls') or None
         if opcao == '1':
             description = input("Descreva a tarefa: ")
             task_manager.add_task(description)
